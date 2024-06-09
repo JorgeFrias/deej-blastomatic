19f79aa1a30b18a7c7583bfed1fd5ecf327ea145
Jorge Frias <jorge@reliby.com>
2024-06-09 23:27:39 +0200

    Disabling leds, enabling pcb sliders.

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,9 +1,24 @@
+#define LEDR 8
+#define LEDG 9
+#define LEDB 7
+
 const int NUM_SLIDERS = 5;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+const int analogInputs[NUM_SLIDERS] = {0, 1, 2, 3, 4};
 
 int analogSliderValues[NUM_SLIDERS];
 
 void setup() { 
+  analogReadResolution(10);       // Set ADC resolution to 10 bits - 0-1023
+  analogSetAttenuation(ADC_11db); // Set ADC input attenuation to 0dB
+
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+
+  digitalWrite(LEDR, LOW);      // Turn off all LEDs
+  digitalWrite(LEDG, LOW);
+  digitalWrite(LEDB, LOW);
+
   for (int i = 0; i < NUM_SLIDERS; i++) {
     pinMode(analogInputs[i], INPUT);
   }
@@ -13,8 +28,8 @@ void setup() {
 
 void loop() {
   updateSliderValues();
-  sendSliderValues(); // Actually send data (all the time)
-  // printSliderValues(); // For debug
+  sendSliderValues();           // Actually send data (all the time)
+  // printSliderValues();       // For debug
   delay(10);
 }
 
